1019852e3aa8b787a1046d40722797a0a7c3ff05
yash8468 <avhadyash922@gmail.com>
2026-03-03 12:00:35 +0000

    comparisons on value

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)            //this comparisons are simple not any problem but all are confusable
+
+// console.log("2">1)             //here they automatically converts string into number
+// console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)              //this type conversions are understand carefully
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)         //in this three cases of undefined that return the false answer
+
+//===
+
+console.log("2"===2)
+
+
