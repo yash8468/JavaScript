61f9c73b5dc482c2fe3f8eeabc6fa3e183a74168
yash8468 <avhadyash922@gmail.com>
2026-03-03 06:15:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=84846466;
+let accountEmail="yash@gmail.com";
+var accountPassword="12345"
+accountCity="jaipur";
+let accountState;                               //undefined
+
+//accountId=4563738   //not allowed
+console.table([accountEmail,accountPassword,accountCity]);
+
+accountEmail="yash84@gamil.com";
+accountPassword="84687";
+accountCity="Ahilyanagar"
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+//prefer not use var because of issue in block scope and functional scope
