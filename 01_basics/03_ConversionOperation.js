aac9a1186f327f6cb3f42376fa755f3170bb6bfa
yash8468 <avhadyash922@gmail.com>
2026-03-03 06:52:51 +0000

    datatype conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+
+let score="Yash";
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);;
+//console.log(typeof(valueInNumber)) 
+
+//33=>number
+//"33abc"=>NaN
+//true=>1
+//false=>0
+
+let loggedIn="Yash";
+// console.log(typeof(loggedIn))
+
+let booleanIsLoggedIn=Boolean(loggedIn);
+// console.log(booleanIsLoggedIn)
+// console.log(typeof(valueInboolean)) 
+
+//1=>true
+//0=>false
+//""=>false
+//"Yash"=>true
+
+let someNumber=33
+// console.log(typeof(someNumber))
+
+let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
