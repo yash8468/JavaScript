5991322c35cd19a863f0e8a51c43c9ec70dbe910
yash8468 <avhadyash922@gmail.com>
2026-03-03 07:16:17 +0000

    operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -29,3 +29,36 @@ let stringNumber=String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof(stringNumber))
 
+//********************************Operations************************************
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="Yash"
+let str2=" Avhad"
+let str3=str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)            //we wrote the string then next number directly concat with that number
+// console.log("2"+1)
+// console.log("2"+2+2)
+// console.log(1+3+"2")            //First we wrote the numbers then firstly that numbers operation will perform and then concating with that last string
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2                     //this is wrong practice=>this type of code not used in the companies
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)
+
+// link to study(prefix and post opeartion on google)
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
