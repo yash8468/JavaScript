a3b6ef4a4c8c33c5d51f1cae8c85fc64a97d2f9f
yash8468 <avhadyash922@gmail.com>
2026-03-03 06:17:35 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all js code as a newer version
+
+//alert(3+3)   we are using a nodeJS ,not a browser
+
+console.log(3
+            +
+             3)  //code redability should be high at the time of we are writing the code avoid this write in in single line
+
+let name="Yash";
+let age=20;
+let isLoggedIn=false;
+let state;                                 //undefined
+
+/*
+   //number->range 2 to power to 53
+   //bigint=>
+   //string=>""
+   //boolean=>true/false
+   //null=>standalone value(type is=>object)
+   //undefined=>
+  //symbol=>unique
+*/
+  //object
+
+  console.log(typeof(undefined))   //undefined
+  console.log(typeof(null))        //object
