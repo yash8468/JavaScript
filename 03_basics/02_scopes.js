cf6ae80206d819dc75d4957d6a7d71e23e45d3c7
yash8468 <avhadyash922@gmail.com>
2026-03-25 07:05:26 +0000

    scope levels and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,3 +15,50 @@ console.log(c);   //c is a accessible outside->this a scope problem of var datat
 console.log(a)    //global scope a will print here
 
 
+function one(){
+
+    const username="Yash";
+
+    function two(){
+        const website="youtube";
+        console.log(username);
+    }
+    // console.log(website);           //error
+    two()
+}
+
+one()
+
+
+if(true){
+
+    const username="yash";
+    if(username==="yash"){
+        const website=" youtube";
+        console.log(username+website);       //username access aloud because they define as a global outside of nested function  
+    }
+
+    // console.log(website);                        //"website variable not aloud to access ouside the scope"
+}
+
+// console.log(username);   //local variable not aloud to access in global space
+
+// ***********Interesting**************
+
+console.log(addone(5))                       
+
+function addone(num){                 //we define a function in this format of "normal function"then =>you call that function in any location in that file without any error(like function is not defined)
+    return num+1;
+}
+
+
+
+console.log(addTwo(8))   //we define a function in the format of "function expression" then =>you call only that function after function declaration not in any location like a "normal function" in that file
+    return num+1;
+
+//function expression
+const addTwo = function(num){
+    return num+2;
+};
+
+console.log(addTwo) //error=>without bracket and parameter we already defined=>function expression not aloud to access
