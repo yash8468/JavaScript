4bc7381e0a71bb57d5a606cbdbb39fdcdb41edb9
yash8468 <avhadyash922@gmail.com>
2026-03-30 17:31:54 +0000

    IIFE concept

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+//IIFE->Immediately invoked  function expressions
+
+(function chai(){
+  //named iife
+  console.log(`DB CONNECTED`)
+})();
+
+
+((name)=>{
+       console.log(`DB Connected two ${name} `)
+})("yash")
+
