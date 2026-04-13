e7cacf11a9235da540e332f496bc122c397ada07
yash8468 <avhadyash922@gmail.com>
2026-04-13 10:50:59 +0000

    project 1 is  done

---
+++ 07_projects/color-changer/action.js
@@ -0,0 +1,35 @@
+const button=document.querySelectorAll(".button")
+const body=document.querySelector("html")
+
+button.forEach(function(button){
+    console.log(button);
+
+    button.addEventListener("click",function(e){
+
+        console.log(e)
+        const color=e.target.id;
+
+        switch(color){
+            case "grey":
+                body.style.backgroundColor=`${color}`
+                break;
+            case "white":
+                body.style.backgroundColor=`${color}`
+                break;
+            case "blue":
+                body.style.backgroundColor=`${color}`
+                break;
+            case "yellow":
+                body.style.backgroundColor=`${color}`
+                break;
+            case "black":
+                body.style.backgroundColor=`${color}`
+                break;
+            default:
+                console.log("click correctly on the box");
+                break;
+        }
+
+    
+    });
+})
